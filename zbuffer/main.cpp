fe88248fe2d0b7ca2206e11cd277a74a5ce93fbd
maomao <zhoukaimao@163.com>
2017-12-16 23:44:22 +0800

    hahah
    hahdha

--- zbuffer/main.cpp
+++ zbuffer/main.cpp
@@ -1,6 +1,7 @@
 #include <opencv2/core/core.hpp>  
 #include <opencv2/highgui/highgui.hpp> 
 #include<iostream>
+#include<fstream>
 #include<vector>
 using namespace std;
 using namespace cv;
@@ -13,7 +14,7 @@ Mat screen;
 
 const int width = 800;
 const int height = 700;
-const int background = 0x000000;
+const int background = 0x0066ccff;
 
 int POLYCNT = 0;//ploygon count
 int EDGECNT = 0;//edge count
@@ -25,6 +26,9 @@ Edge* ET[height];
 vector<Edge*> AET;
 vector<Poly*> PT;
 vector<Poly*> IPL;
+
+vector<float[3]> vertices;
+vector<vector<int> > indices;
 
 
 struct vec3f {
@@ -47,20 +51,27 @@ struct Poly {
 };
 struct Edge {
 	int ID;
-	int polyId;
 	float x;//上端点的x坐标
 	float dx;//斜率是k,dx则是-1/k
 	float dy;//边跨越的扫描线数目
 	int ymax;
 
+	Poly* ply;
 	Edge* next;
 	Edge() { ID = EDGECNT++; }
 };
 
 
-
-void draw_line(int x1, int x2,int color) {
-
+Vec3b int2color(int color) {
+	Vec3b c;
+	c[2] = (color & 0x00ff0000) >> 16;
+	c[1] = (color & 0x0000ff00) >> 8;
+	c[0] = color & 0x000000ff;
+	return c;
+}
+void draw_line(int y, int x1, int x2,int color) {
+	Vec3b c = int2color(color);
+	screen(Rect(x1, y, x2, y)) = c;
 }
 void build_ply() {
 
@@ -71,22 +82,35 @@ void update() {
 
 void init() {
 	screen = Mat::zeros(height, width, CV_8UC3);
-	screen.setTo(0);
+	screen = int2color(background);
+
 	BG = new Poly();
 	X0 = new Edge();
 	X1 = new Edge();
 	X0->x = 0;
-	X0->ymax = height;
+	X0->ymax = height-1;
 	X0->dx = 0;
-	X0->polyId = ;
-	X
+	X0->ply = BG;
+	X1->x = width - 1;
+	X1->ymax = height - 1;
+	X1->dx = 0;
+	X1->ply = BG;
+	X0->next = X1;
+	BG->firste = X0;
+	BG->color = background;
+
 }
 void load(string filepath) {
+	ifstream is(filepath);
+	if (!is.is_open()) {
+		cout << "fail to open file1" << endl;
+		exit(1);
+	}
 
 }
 void scan(int ymin = 0, int ymax = height) {
-	build_ET();
-	build_PT();
+	//build_ET();
+	//build_PT();
 
 	for (int y = ymin; y < ymax; y++) {
 
@@ -95,6 +119,8 @@ void scan(int ymin = 0, int ymax = height) {
 int main() {
 	init();
 	load("example.poly");
+	build_ET();
+	build_PT();
 	scan();
 
 	imshow("show", screen);
